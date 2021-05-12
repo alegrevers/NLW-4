bf57a07739d8efcbd22b61305e7a1813dcdf82c9
gabrielalegrete <gabrielalegrete1@gmail.com>
2021-05-12 18:56:19 -0300

    relacionamentos com banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import 'reflect-metadata'
+import './database'
 import express, { response } from 'express';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.get("/", (request, response) => {
     // return response.send("salve meu peixe");
